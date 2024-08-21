303c72ee5250fc8dbdd580572f0978c0d0298fff
rakshavinod <inbox.rakshav@gmail.com>
2024-08-21 20:56:09 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; 
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "tailwindcss/tailwind.css";
 
 console.log("hi");
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
